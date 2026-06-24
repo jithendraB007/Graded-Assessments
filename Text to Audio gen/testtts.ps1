41d233bdc05628514f3af16b37495b86d16fdff7
English Products <english-team@nxtwave.co.in>
2026-06-24 10:50:44 +0530

    Delete Text to Audio gen directory

--- Text to Audio gen/testtts.ps1
+++
@@ -1,6 +0,0 @@
-Add-Type -AssemblyName System.Speech
-$synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$synth.GetInstalledVoices() | ForEach-Object { Write-Output $_.VoiceInfo.Name }
-$synth.SetOutputToWaveFile("test.wav")
-$synth.Speak("Testing 1 2 3")
-$synth.Dispose()
